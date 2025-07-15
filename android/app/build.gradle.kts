cec9a58accecf4711972f8b8141dd96aca49748d
Mathew131 <mathew.kurchenko23@gmail.com>
2025-07-15 15:39:01 +0300

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 }
 
 android {
-    namespace = "com.example.archery"
+    namespace = "com.mathew.archerydiary"
     compileSdk = flutter.compileSdkVersion
 //    ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.archery"
+        applicationId = "com.mathew.archerydiary"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 //        minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/mathew/archerydiary/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mathew.archerydiary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {}
