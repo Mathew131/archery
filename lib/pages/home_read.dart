92af640b4a200268b8028e364e307b6d130875cb
Mathew131 <mathew.kurchenko23@gmail.com>
2025-07-31 22:48:51 +0300

    hidden home-read

--- lib/pages/home_read.dart
+++ lib/pages/home_read.dart
@@ -17,6 +17,22 @@ class _HomeReadState extends State<HomeRead> {
   String name_note = 'Новая запись';
   String selectedDistance = 'Дистанция: 18м';
   String selectedFilterDistance = 'Все дистанции';
+  Map<String, bool> isVisible = {};
+  Map<String, String> month = {
+    '01': 'Январь',
+    '02': 'Февраль',
+    '03': 'Март',
+    '04': 'Апрель',
+    '05': 'Май',
+    '06': 'Июнь',
+    '07': 'Июль',
+    '08': 'Август',
+    '09': 'Сентябрь',
+    '10': 'Октябрь',
+    '11': 'Ноябрь',
+    '12': 'Декабрь',
+  };
+
 
   @override
   void didChangeDependencies() {
@@ -53,85 +69,140 @@ class _HomeReadState extends State<HomeRead> {
     }
   }
 
-  Widget button(BuildContext context, int index) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(16, 6, 16, 6),
-      child: ElevatedButton(
+  Widget? getMonthHeader(int index) {
+    String currentMonthYear = current_notes[index].split('_')[2].split('.').sublist(1, 3).join('.');
+
+    if (index == 0 || currentMonthYear != current_notes[index-1].split('_')[2].split('.').sublist(1, 3).join('.')) {
+
+      return ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.orangeAccent.shade100,
+          backgroundColor: Colors.grey.shade100,
           padding: EdgeInsets.symmetric(vertical: 12),
-          elevation: 2,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.zero,
+            side: BorderSide(
+              color: Colors.grey.shade200,
+              width: 1,
+            ),
           ),
         ),
-
-        onPressed: () async {
-          sl<Data>().current_name = current_notes[index];
-          await Navigator.pushNamed(context, '/table', arguments: 'r');
-
+        onPressed: () {
           setState(() {
-            notes = sl<Data>().getNotes();
-            current_notes = sl<Data>().getNotes();
+            isVisible[currentMonthYear] = isVisible.containsKey(currentMonthYear) ? !isVisible[currentMonthYear]! : false;
           });
         },
-
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        current_notes[index].split('_')[0],
-                        style: TextStyle(color: Colors.black, fontSize: 16),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        current_notes[index].split('_').sublist(1, 3).join(' '),
-                        style: TextStyle(color: Colors.black54, fontSize: 12),
-                      ),
-                    ],
-                  )
-              ),
-            ),
-
-
             Padding(
-              padding: EdgeInsets.only(left: 10, right: 20),
-              child: Row(
-                children: [
-                  Text(
-                    '${current_notes[index].split('_')[4]}',
-                    style: TextStyle(color: current_notes[index].split('_')[6] == 'true' ? Color(0xFF4c8f28) : Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    ' + ',
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                  Text(
-                    '${current_notes[index].split('_')[5]}',
-                    style: TextStyle(color: current_notes[index].split('_')[7] == 'true' ? Color(0xFF4c8f28) : Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    ' = ',
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                  Text(
-                    '${int.parse(current_notes[index].split('_')[4]) + int.parse(current_notes[index].split('_')[5])}',
-                    style: TextStyle(color: current_notes[index].split('_')[6] == 'true' && current_notes[index].split('_')[7] == 'true' ? Color(0xFF4c8f28) : Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ],
+              padding: EdgeInsets.fromLTRB(36, 0, 36, 0),
+              child: Text(
+                '${month[currentMonthYear.split('.')[0]]} ${currentMonthYear.split('.')[1]}',
+                style: TextStyle(fontSize: 16),
               ),
             ),
-          ]
-        )
-      ),
-    );
+            Padding(
+              padding: EdgeInsets.fromLTRB(36, 0, 36, 0),
+              child:  Icon(
+                (isVisible.containsKey(currentMonthYear) ? !isVisible[currentMonthYear]! : false) ? Icons.expand_more : Icons.expand_less,
+                // color: Colors.black,
+                size: 23,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return null;
+  }
+
+  Widget? getButton(BuildContext context, int index) {
+    final key = isVisible[current_notes[index].split('_')[2].split('.').sublist(1, 3).join('.')] ?? true;
+
+    if (key) {
+      return Padding(
+        padding: EdgeInsets.fromLTRB(16, 6, 16, 6),
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orangeAccent.shade100,
+              padding: EdgeInsets.symmetric(vertical: 12),
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+
+            onPressed: () async {
+              sl<Data>().current_name = current_notes[index];
+              await Navigator.pushNamed(context, '/table', arguments: 'r');
+
+              setState(() {
+                notes = sl<Data>().getNotes();
+                current_notes = sl<Data>().getNotes();
+              });
+            },
+
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              current_notes[index].split('_')[0],
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              current_notes[index].split('_').sublist(1, 3).join(' '),
+                              style: TextStyle(color: Colors.black54, fontSize: 12),
+                            ),
+                          ],
+                        )
+                    ),
+                  ),
+
+
+                  Padding(
+                    padding: EdgeInsets.only(left: 10, right: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          '${current_notes[index].split('_')[4]}',
+                          style: TextStyle(color: current_notes[index].split('_')[6] == 'true' ? Color(0xFF4c8f28) : Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          ' + ',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        Text(
+                          '${current_notes[index].split('_')[5]}',
+                          style: TextStyle(color: current_notes[index].split('_')[7] == 'true' ? Color(0xFF4c8f28) : Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          ' = ',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        Text(
+                          '${int.parse(current_notes[index].split('_')[4]) + int.parse(current_notes[index].split('_')[5])}',
+                          style: TextStyle(color: current_notes[index].split('_')[6] == 'true' && current_notes[index].split('_')[7] == 'true' ? Color(0xFF4c8f28) : Colors.red, fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]
+            )
+        ),
+      );
+    } else {
+      return null;
+    }
+
   }
 
   @override
@@ -145,7 +216,7 @@ class _HomeReadState extends State<HomeRead> {
         children: [
           Center(
             child: Opacity(
-              opacity: .2,
+              opacity: 0.2,
               child: Image.asset('assets/arch.jpg', width: 300, height: 300),
             ),
           ),
@@ -206,7 +277,19 @@ class _HomeReadState extends State<HomeRead> {
             body: ListView.builder(
               padding: EdgeInsets.symmetric(vertical: 6),
               itemCount: current_notes.length,
-              itemBuilder: (c, i) => button(c, i),
+              itemBuilder: (context, index) {
+                final monthHeader = getMonthHeader(index);
+                final button = getButton(context, index);
+
+                return Column(
+                  children: [
+                    if (monthHeader != null) monthHeader,
+                    if (button != null) ... [
+                      button,
+                    ] else if (index == current_notes.length - 1) SizedBox(height: 90,),
+                  ],
+                );
+              },
             ),
           ),
         ],
@@ -220,14 +303,14 @@ class _NameBanner extends SliverPersistentHeaderDelegate {
   _NameBanner(this.name);
 
   @override
-  double get minExtent => 40;
+  double get minExtent => 49;
   @override
-  double get maxExtent => 40;
+  double get maxExtent => 49;
 
   @override
   Widget build(BuildContext context, double shrink, bool overlaps) {
     return Material(
-      elevation: 2,
+      elevation: 1,
       color: Colors.grey.shade100,
       child: Container(
         alignment: Alignment.center,
